692f0a5659f1b13b50f13ace67519d786e893cb9
Fengck9521 <2207597682@qq.com>
2024-10-09 16:27:34 +0800

    fix: likes api

---
+++ db/init.sql
@@ -0,0 +1,7 @@
+insert into tags(id,created_at)values('73945612',datetime());
+insert into tags(id,created_at)values('28106534',datetime());
+insert into tags(id,created_at)values('59873021',datetime());
+insert into tags(id,created_at)values('41682907',datetime());
+insert into tags(id,created_at)values('86230159',datetime());
+insert into tags(id,created_at)values('37594820',datetime());
+
